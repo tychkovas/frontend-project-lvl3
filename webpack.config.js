0b1b637929f945b550d8e5c13f02342208cc2fe5
Aleksandr T <tychkovas@yandex.ru>
2023-04-05 07:48:37 +0300

    feat(webpack): add config from webpack-cli/generators and add output ts - add remarks

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,57 @@
-const path = require('path');
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const mode = process.env.NODE_ENV || 'development';
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode,
-  entry: './src/index.ts',
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
   },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   }
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
 };
