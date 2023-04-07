0293a2213cc822fcea124d615fbfcaaf25cea94c
tychkovas <tychkovas@yandex.ru>
2023-04-07 18:45:35 +0300

    feat(app): add url output to console

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
+// import * as yup from 'yup';
+// import onChange from 'on-change';
+// import * as _ from 'lodash';
+// import _ from 'lodash';
+
 export default () => {
+  const elements = {
+    form: document.querySelector('.rss-form') as HTMLInputElement,
+    input: document.querySelector('#url-input') as HTMLInputElement
+  };
+
+  elements.form.addEventListener('submit', (e: SubmitEvent) => {
+    e.preventDefault();
+    const data: FormData = new FormData(e.target as HTMLFormElement);
+    const currentURL = data.get('url');
+    console.log('currentURL =', currentURL);
+  });
+
   console.log('Hello World!');
 };
